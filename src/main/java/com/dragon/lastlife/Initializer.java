a2b2c30170b29b3a3bf47097d5ab02b8e427edd2
QuickScythe <QuickScythe@gmail.com>
2025-08-10 12:11:34 -0500

    init commit

---
+++ src/main/java/com/dragon/lastlife/Initializer.java
@@ -0,0 +1,17 @@
+package com.dragon.lastlife;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Initializer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
