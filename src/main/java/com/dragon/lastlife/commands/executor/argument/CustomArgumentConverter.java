12dbd353ac91e3a768a1cb7c6060f2d492bffd73
QuickScythe <quickscythe@gmail.com>
2025-12-15 04:10:20 -0600

    Refactor `CustomArgumentConverter` to streamline `onlySimilar` method and improve null handling in suggestions logic

--- src/main/java/com/dragon/lastlife/commands/executor/argument/CustomArgumentConverter.java
+++ src/main/java/com/dragon/lastlife/commands/executor/argument/CustomArgumentConverter.java
@@ -1,6 +1,5 @@
 package com.dragon.lastlife.commands.executor.argument;
 
-import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
@@ -10,22 +9,24 @@ import java.util.concurrent.CompletableFuture;
 
 public abstract class CustomArgumentConverter<R, I> implements CustomArgumentType.Converted<@NotNull R, @NotNull I> {
 
-    public CompletableFuture<Suggestions> onlySimilar(String[] values, String input, SuggestionsBuilder builder) {
-        return getSuggestionsCompletableFuture(values, input, builder);
-    }
-
     public static CompletableFuture<Suggestions> getSuggestionsCompletableFuture(String[] values, String input, SuggestionsBuilder builder) {
         if (input == null || input.isBlank()) {
             for (String v : values) {
+                if (v == null) continue;
                 builder.suggest(v);
             }
             return builder.buildFuture();
         }
         for (String v : values) {
+            if (v == null) continue;
             if (v.toLowerCase().startsWith(input.toLowerCase())) {
                 builder.suggest(v);
             }
         }
         return builder.buildFuture();
+    }
+
+    public CompletableFuture<Suggestions> onlySimilar(String[] values, String input, SuggestionsBuilder builder) {
+        return getSuggestionsCompletableFuture(values, input, builder);
     }
 }
