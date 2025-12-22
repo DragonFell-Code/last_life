8d4e7ad53024e2cde29256a4e3e60b764ce9f62f
QuickScythe <quickscythe@gmail.com>
2025-12-22 03:37:02 -0600

    Refactor `Factories` to simplify registration logic; remove unused `ConfigStringFactory` and related code.

--- src/main/java/com/dragon/lastlife/config/factories/Factories.java
+++ src/main/java/com/dragon/lastlife/config/factories/Factories.java
@@ -1,6 +1,5 @@
 package com.dragon.lastlife.config.factories;
 
-import com.dragon.lastlife.config.object.ConfigLocation;
 import com.dragon.lastlife.donations.Donation;
 import com.dragon.lastlife.party.Party;
 import com.dragon.lastlife.players.Participant;
@@ -9,22 +8,22 @@ import com.quiptmc.core.config.ConfigManager;
 import com.quiptmc.core.config.ConfigObject;
 import com.quiptmc.core.config.factories.ConfigStringFactory;
 import com.quiptmc.core.config.factories.GenericFactory;
-import com.quiptmc.core.config.objects.ConfigString;
 import com.quiptmc.core.discord.Webhook;
 
 public class Factories {
 
-    public final ConfigObject.Factory<Participant> PARTICIPANT_FACTORY = register(new GenericFactory<>(Participant.class));
-    public final ConfigObject.Factory<Donation> DONATION_FACTORY = register(new GenericFactory<>(Donation.class));
-    public final ConfigObject.Factory<Webhook> WEBHOOK_FACTORY = register(new GenericFactory<>(Webhook .class));
-    public final ConfigObject.Factory<Party> PARTY_FACTORY = register(new GenericFactory<>(Party.class));
-    public final ConfigObject.Factory<ConfigString> CONFIG_STRING_FACTORY = register(new ConfigStringFactory());
-    public final ConfigObject.Factory<ConfigLocation> CONFIG_LOCATION_FACTORY = register(new ConfigLocationFactory());
+    public Factories() {
+        register(new GenericFactory<>(Participant.class));
+        register(new GenericFactory<>(Donation.class));
+        register(new GenericFactory<>(Webhook.class));
+        register(new GenericFactory<>(Party.class));
+        register(new ConfigStringFactory());
+        register(new ConfigLocationFactory());
+    }
 
-    public <T extends ConfigObject.Factory<?>> T register(T factory) {
+    public <T extends ConfigObject.Factory<?>> void register(T factory) {
         Utils.initializer().integration().log("Factories", "Registering factory: " + factory.getClass().getName());
         ConfigManager.registerFactory(factory);
-        return factory;
     }
 
 }
